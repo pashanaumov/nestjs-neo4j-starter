2046f656d0e4ed9b165516740abe63af92c4e3ee
Pavel Naumov <36989187+pavelnaumov@users.noreply.github.com>
2021-11-18 22:14:01 +0300

    Initial commit

---
+++ src/auth/encryption/encryption.service.spec.ts
@@ -0,0 +1,19 @@
+import { ConfigService } from '@nestjs/config';
+import { Test, TestingModule } from '@nestjs/testing';
+import { EncryptionService } from './encryption.service';
+
+describe('EncryptionService', () => {
+  let service: EncryptionService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [ConfigService, EncryptionService],
+    }).compile();
+
+    service = module.get<EncryptionService>(EncryptionService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
